9714aa368061c6e220349d216c44d06f168a7ebf
Juan Paez <juanfranciscopaez@gmail.com>
2021-11-02 22:50:46 -0500

    add threading to support key holding

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import blynklib
-import ctypes
-from ctypes import wintypes
-import time
 import time
 import auth_vars
 from pyautogui import press, typewrite, hotkey, keyDown, keyUp
+import threading
 
 # ******************** SETUP ********************
 key_bindings = {
@@ -22,92 +20,103 @@ key_bindings = {
 
 BLYNK_AUTH = auth_vars.BLYNK_AUTH
 blynk = blynklib.Blynk(BLYNK_AUTH)
-
 WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_PIN_EVENT] Pin: V{} Value: '{}'"
 CONNECT_PRINT_MSG = '[CONNECT_EVENT]'
+
+key_to_press = None
 
 # ******************** SPEED HANDLERS ********************
 @blynk.handle_event('write V1')
 def handle_joystick_y(pin, value):
     print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+    global key_to_press
     if value[0] == '1':
-        keyDown(key_bindings["SPEED_1"])
+        key_to_press = key_bindings["SPEED_1"]
     else:
-        keyUp(key_bindings["SPEED_1"])
+        key_to_press = None
 
 @blynk.handle_event('write V2')
 def handle_joystick_y(pin, value):
     print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+    global key_to_press
     if value[0] == '1':
-        keyDown(key_bindings["SPEED_2"])
+        key_to_press = key_bindings["SPEED_2"]
     else:
-        keyUp(key_bindings["SPEED_2"])
+        key_to_press = None
 
 @blynk.handle_event('write V3')
 def handle_joystick_y(pin, value):
     print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+    global key_to_press
     if value[0] == '1':
-        keyDown(key_bindings["SPEED_3"])
+        key_to_press = key_bindings["SPEED_3"]
     else:
-        keyUp(key_bindings["SPEED_3"])
+        key_to_press = None
 
 @blynk.handle_event('write V4')
 def handle_joystick_y(pin, value):
     print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+    global key_to_press
     if value[0] == '1':
-        keyDown(key_bindings["SPEED_4"])
+        key_to_press = key_bindings["SPEED_4"]
     else:
-        keyUp(key_bindings["SPEED_4"])
+        key_to_press = None
 
 # ******************** DIRECTION HANDLERS ********************
 @blynk.handle_event('write V5')
 def handle_joystick_y(pin, value):
     print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+    global key_to_press
     if value[0] == '1':
-        keyDown(key_bindings["ARROW_UP"])
+        key_to_press = key_bindings["ARROW_UP"]
     else:
-        keyUp(key_bindings["ARROW_UP"])
+        key_to_press = None
 
 @blynk.handle_event('write V6')
 def handle_joystick_y(pin, value):
     print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+    global key_to_press
     if value[0] == '1':
-        keyDown(key_bindings["ARROW_DOWN"])
+        key_to_press = key_bindings["ARROW_DOWN"]
     else:
-        keyUp(key_bindings["ARROW_DOWN"])
+        key_to_press = None
 
 @blynk.handle_event('write V7')
 def handle_joystick_y(pin, value):
     print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+    global key_to_press
     if value[0] == '1':
-        keyDown(key_bindings["ARROW_LEFT"])
+        key_to_press = key_bindings["ARROW_LEFT"]
     else:
-        keyUp(key_bindings["ARROW_LEFT"])
+        key_to_press = None
 
 @blynk.handle_event('write V8')
 def handle_joystick_y(pin, value):
     print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+    global key_to_press
     if value[0] == '1':
-        keyDown(key_bindings["ARROW_RIGHT"])
+        key_to_press = key_bindings["ARROW_RIGHT"]
     else:
-        keyUp(key_bindings["ARROW_RIGHT"])
+        key_to_press = None
 
 # ******************** Z AXIS HANDLERS ********************
 @blynk.handle_event('write V9')
 def handle_joystick_y(pin, value):
     print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+    global key_to_press
     if value[0] == '1':
-        keyDown(key_bindings["Z_DOWN"])
+        key_to_press = key_bindings["Z_DOWN"]
     else:
-        keyUp(key_bindings["Z_DOWN"])
+        key_to_press = None
 
 @blynk.handle_event('write V10')
 def handle_joystick_y(pin, value):
     print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+    global key_to_press
     if value[0] == '1':
-        keyDown(key_bindings["Z_UP"])
+        key_to_press = key_bindings["Z_UP"]
     else:
-        keyUp(key_bindings["Z_UP"])
+        key_to_press = None
     
 # ******************** CONNECTION HANDLER ********************
 @blynk.handle_event("connect")
@@ -119,8 +128,17 @@ def connect_handler():
 
 # ******************** MAIN LOOP ********************
 def main():
+    x = threading.Thread(target=press_thread, daemon=True)
+    x.start()
     while True:
         blynk.run()
+        #if key_to_press is not None:
+            #press(key_to_press)
+
+def press_thread():
+    while True:
+        if key_to_press is not None:
+            press(key_to_press)
 
 if __name__ == "__main__":
     main()
