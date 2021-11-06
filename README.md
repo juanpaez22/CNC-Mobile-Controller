b91082185973f05a90752557522b3f4ab24e87ee
Juan Paez <juanfranciscopaez@gmail.com>
2021-11-06 13:33:47 -0500

    add hold and press modes

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # CNC-Mobile-Controller
 
-This Python client receives inputs from a custom project in the Blynk mobile app and uses them to simulate keyboard clicks. This allows for remote interaction with CNC router software using a mobile device rather than the physical computer keyboard.
+This Python client receives inputs from a custom project in the Blynk mobile app and uses them to simulate keyboard clicks for input into the Carbide Motion jogging pane. This allows for remote interaction with CNC router using a mobile device rather than the physical computer keyboard.
+
+![Blynk App](imgs/blynk.jpg)
+
+To use the same Blynk project template, use this QR code:
+![Blynk QR](imgs/qr.jpg)
+
 
 Virtual Pin - Key Mappings:
 - V1: Speed 1
 - V2: Speed 2
 - V3: Speed 3
-- V4: Speed 4
+- V4: Toggle hold/press mode
 - V5: Direction up
 - V6: Direction down
 - V7: Direction left
@@ -17,6 +23,6 @@ Virtual Pin - Key Mappings:
 To use this client:
 1. Create a Blynk project that uses buttons mapped to each of the virtual pins listed above
 2. Copy the blynk authorization token into a file called "auth_vars.py" as follows: `BLYNK_AUTH = "<your auth token>"`
-3. From a terminal in the current directory, run `python3 app.py`
-4. Bring the CNC software window to the front
+3. From a terminal in the current directory, install dependencies using `pip install -r requirements.txt`, and then run `python3 app.py`
+4. Bring Carbide Motion software window to the front in the jogging tab
 5. Use the controls in the Blynk app to control CNC remotely
--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import blynklib
 import time
 import auth_vars
-from pyautogui import press, typewrite, hotkey, keyDown, keyUp
+from pyautogui import press
 import threading
 
 # ******************** SETUP ********************
 key_bindings = {
-    1: "1",         # Pin V1 -> Speed 1
-    2: "2",         # Pin V2 -> Speed 2
-    3: "3",         # Pin V3 -> Speed 3
-    4: "4",         # Pin V4 -> Speed 4
+    1: "1",         # Pin V1 -> Speed 1 (0.025 mm)
+    2: "2",         # Pin V2 -> Speed 2 (0.25 mm)
+    3: "3",         # Pin V3 -> Speed 3 (1 mm)
+    4: None,        # Pin V4 -> Change mode (single vs. hold)
     5: "up",        # Pin V5 -> Arrow up
     6: "down",      # Pin V6 -> Arrow down
     7: "left",      # Pin V7 -> Arrow left
@@ -23,69 +23,81 @@ blynk = blynklib.Blynk(BLYNK_AUTH)
 WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_PIN_EVENT] Pin: V{} Value: '{}'"
 CONNECT_PRINT_MSG = '[CONNECT_EVENT]'
 
-key_to_press = None
+mode_hold = True        # Hold or single press
+key_to_hold = None     # Which key to press in background thread
 
 # ******************** CONTROL HELPERS ********************
-def handle_press_event(pin, value):
+def handle_pin_event(pin, value):
     print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+    global key_to_hold
+    global mode_hold
     if value[0] == '1':
-        press(key_bindings[pin])
-
-def handle_hold_event(pin, value):
-    print(WRITE_EVENT_PRINT_MSG.format(pin, value))
-    global key_to_press
-    if value[0] == '1':
-        key_to_press = None
-        keyDown(key_bindings[pin])
+        if pin == 4:
+            # Set single press mode
+            key_to_hold = None
+            mode_hold = False
+        elif mode_hold:
+            # Set key to hold using key binding map
+            key_to_hold = key_bindings[pin]
+        else:
+            # Press single key using key binding map
+            press(key_bindings[pin])
     else:
-        key_to_press = None
-        keyUp(key_bindings[pin])
+        if pin == 4:
+            # Set hold mode
+            key_to_hold = None
+            mode_hold = True
+        elif mode_hold:
+            # Stop holding key
+            key_to_hold = None
+        else:
+            pass
 
-def press_thread():
+def hold_thread():
     while True:
-        if key_to_press is not None:
-            press(key_to_press)
+        if key_to_hold is not None:
+            press(key_to_hold)
 
 # ******************** VIRTUAL PIN HANDLERS ********************
 @blynk.handle_event('write V1')
 def handle_pin(pin, value):
-    handle_press_event(pin, value)
+    handle_pin_event(pin, value)
 
 @blynk.handle_event('write V2')
 def handle_pin(pin, value):
-    handle_press_event(pin, value)
+    handle_pin_event(pin, value)
 
 @blynk.handle_event('write V3')
 def handle_pin(pin, value):
-    handle_press_event(pin, value)
+    handle_pin_event(pin, value)
 
 @blynk.handle_event('write V4')
 def handle_pin(pin, value):
-    handle_press_event(pin, value)
+    handle_pin_event(pin, value)
 
 @blynk.handle_event('write V5')
 def handle_pin(pin, value):
-    handle_press_event(pin, value)
+    handle_pin_event(pin, value)
 
 @blynk.handle_event('write V6')
 def handle_pin(pin, value):
-    handle_press_event(pin, value)
+    handle_pin_event(pin, value)
 
 @blynk.handle_event('write V7')
 def handle_pin(pin, value):
-    handle_press_event(pin, value)
+    handle_pin_event(pin, value)
 
 @blynk.handle_event('write V8')
 def handle_pin(pin, value):
-    handle_press_event(pin, value)
+    handle_pin_event(pin, value)
 
 @blynk.handle_event('write V9')
 def handle_pin(pin, value):
-    handle_press_event(pin, value)
+    handle_pin_event(pin, value)
 
 @blynk.handle_event('write V10')
 def handle_pin(pin, value):
-    handle_press_event(pin, value)
+    handle_pin_event(pin, value)
     
 # ******************** CONNECTION HANDLER ********************
 @blynk.handle_event("connect")
@@ -97,8 +109,8 @@ def connect_handler():
 
 # ******************** MAIN LOOP ********************
 def main():
-    # x = threading.Thread(target=press_thread, daemon=True)
-    # x.start()
+    t = threading.Thread(target=hold_thread, daemon=True)
+    t.start()
     while True:
         blynk.run()
 
